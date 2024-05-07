1c9dd846eee756acc6860967b8dfb0e091e85bf7
雨碎风希 <2078389729@qq.com>
2024-05-07 23:50:23 +0800

    matchDemo6-4

---
+++ study6.2/matchDemo6-4/src/main.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)] // 这样可以立刻看到州的名称
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {}
