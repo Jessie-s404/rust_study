d0d90fd8d8524b4904634f5788107d83fca2cfdb
雨碎风希 <2078389729@qq.com>
2024-05-08 23:41:51 +0800

    UsState

--- study6.2/matchDemo6-4/src/main.rs
+++ study6.2/matchDemo6-4/src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)] // 这样可以立刻看到州的名称
+#[derive(Debug)]
 enum UsState {
     Alabama,
     Alaska,
@@ -12,4 +12,18 @@ enum Coin {
     Quarter(UsState),
 }
 
-fn main() {}
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
